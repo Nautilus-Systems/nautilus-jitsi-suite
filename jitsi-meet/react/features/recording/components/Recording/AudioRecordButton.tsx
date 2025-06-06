cdf11c7eaf032d18133124d260f8dc15539f8b63
Alison Martins <alison.martins@varsitytutors.com>
2025-06-06 18:20:47 -0300

    Fix AudioRecordButton TypeScript errors - update imports and implementation

--- jitsi-meet/react/features/recording/components/Recording/AudioRecordButton.tsx
+++ jitsi-meet/react/features/recording/components/Recording/AudioRecordButton.tsx
@@ -1,19 +1,16 @@
 /* eslint-disable lines-around-comment */
 import React, { useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { createToolbarEvent } from '../../../analytics/AnalyticsEvents';
 import { sendAnalytics } from '../../../analytics/functions';
 import { IReduxState } from '../../../app/types';
-import { IconMicrophone } from '../../../base/icons/svg';
+import { getCurrentConference } from '../../../base/conference/functions';
+import { IconMic, IconRecord, IconStop } from '../../../base/icons/svg';
+import { JitsiRecordingConstants } from '../../../base/lib-jitsi-meet';
 import Button from '../../../base/ui/components/web/Button';
 import { BUTTON_TYPES } from '../../../base/ui/constants.web';
-import { useSelector } from '../../../base/util/reactUtils';
-// @ts-ignore
-import { JitsiRecordingConstants } from '../../../base/lib-jitsi-meet';
-
-import { useAudioRecordingButton } from '../../hooks.web';
 
 /**
  * The type of the React {@code Component} props of {@link AudioRecordButton}.
@@ -41,30 +38,56 @@ const AudioRecordButton = ({ _tooltip, disabled }: IProps): JSX.Element => {
     const dispatch = useDispatch();
     const { t } = useTranslation();
 
-    const {
-        isRecording,
-        isSupported,
-        onClick
-    } = useAudioRecordingButton();
+    // Get recording state from Redux
+    const { sessionDatas } = useSelector((state: IReduxState) => state['features/recording']);
+    const conference = useSelector(getCurrentConference);
+    
+    const isRecording = sessionDatas.some(
+        (recording: any) => recording.status === JitsiRecordingConstants.status.ON
+    );
 
     const handleClick = useCallback(() => {
         sendAnalytics(createToolbarEvent('audiorecording'));
-        onClick();
-    }, [ onClick ]);
+        
+        if (isRecording) {
+            // Stop the active recording session
+            const activeSession = sessionDatas?.find((session: any) =>
+                session.status === JitsiRecordingConstants.status.ON
+            );
 
-    if (!isSupported) {
+            if (activeSession && conference) {
+                conference.stopRecording(activeSession.id);
+            }
+        } else {
+            // Start audio-only recording
+            if (conference) {
+                conference.startRecording({
+                    mode: JitsiRecordingConstants.mode.FILE,
+                    audioOnly: true,
+                    appData: JSON.stringify({
+                        'file_recording_metadata': {
+                            audioOnly: true
+                        }
+                    })
+                });
+            }
+        }
+    }, [isRecording, sessionDatas, conference]);
+
+    // Don't render if no conference
+    if (!conference) {
         return <></>;
     }
 
     const tooltip = _tooltip || (isRecording 
-        ? t('recording.stopAudioRecording') 
-        : t('recording.startAudioRecording'));
+        ? t('dialog.stopAudioRecording') 
+        : t('dialog.startAudioRecording'));
 
     return (
         <Button
             accessibilityLabel = { tooltip }
             disabled = { disabled }
-            icon = { IconMicrophone }
+            icon = { isRecording ? IconStop : IconRecord }
             onClick = { handleClick }
             toggled = { isRecording }
             tooltip = { tooltip }
